5035fd317a023cd567384569fec5d82ee6de4ff5
PRATHYUSHCC <118652844+PRATHYUSHCC@users.noreply.github.com>
2024-10-27 12:59:36 +0530

    Update NewSelection.java

--- DOP/src/newshelf/NewSelection.java
+++ DOP/src/newshelf/NewSelection.java
@@ -13,9 +13,21 @@ public class NewSelection {
 	public static String getAgeOrTitle(Object o) {
 		
 		if (o instanceof Book) {
-			
+			if(o instanceof Comic){
+				Comic comic=(Comic) o;
+				return comic.title;
+			}
+			else if(o instanceof Fiction){
+				Fiction fiction=(Fiction) o;
+				return fiction.name;
+			}
+			else if(o instanceof TextBook){
+				TextBook text=(TextBook) o;
+				return text.subject;
+			}
 		}
 		return null;
+	}
 	}
 
 	public static void main(String[] args) {
