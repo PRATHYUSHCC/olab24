3d2840fc432b2d8c63b054e21ba3cf06223e815f
PRATHYUSHCC <118652844+PRATHYUSHCC@users.noreply.github.com>
2024-10-27 19:39:52 +0530

    Update Fiction.java

--- DOP/src/oldshelf/Fiction.java
+++ DOP/src/oldshelf/Fiction.java
@@ -2,18 +2,26 @@ package oldshelf;
 
 public class Fiction extends Book {
 
-	/* TODO: Add most strict modifiers here*/ String name;
-	// TODO: change 
-	/**
-	 * type is  a per instance object and it is initialized only once.
-	 * 
-	 */
-	private final String name;
-	private static FictionType type;
-	public Fiction(String title,String name) {
-		super(title);
-		this.name=name;
-		// TODO  correct the above.
-	}
+    private final String name;
 
+    private final FictionType type;
+
+    public Fiction(String name, FictionType type) {
+        super(name);
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public FictionType getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "Fiction{name='" + name + "', type=" + type + "}";
+    }
 }
