d1b89f76b4695df25ddbcf4a695195a2c6b8d126
PRATHYUSHCC <118652844+PRATHYUSHCC@users.noreply.github.com>
2024-10-27 12:55:52 +0530

    Update TextBook.java

--- DOP/src/TextBook.java
+++ DOP/src/TextBook.java
@@ -1,5 +1,22 @@
 
 // TODO: Complete this with record components
-public record TextBook() {
+public record TextBook(String title, String author, int publicationYear, String isbn) {
+    @Override
+    public String toString() {
+        return String.format("Title='%s', author='%s', year=%d, ISBN='%s'",title, author, publicationYear, isbn);
+    }
+
+    public String gettit() {
+        return title;
+    }
+    public String getAuth() {
+        return author;
+    }
+    public int getYear() {
+        return publicationYear;
+    }
+    public String getISBN() {
+        return isbn;
+    }
 
 }
