219bcb9ebca8923d3cd07ee7a397cf8aa49ed1e2
PRATHYUSHCC <118652844+PRATHYUSHCC@users.noreply.github.com>
2024-10-27 19:40:58 +0530

    Update OldSelection.java

--- DOP/src/oldshelf/OldSelection.java
+++ DOP/src/oldshelf/OldSelection.java
@@ -1,33 +1,29 @@
 package oldshelf;
-
 public class OldSelection {
-	
-public static String getAgeOrTitle(Object o) {
-		
-		if (o instanceof Book) {
-			if(o instanceof Comic){
-				Comic comic=(Comic) o;
-				return comic.title;
-			}
-			else if(o instanceof Fiction){
-				Fiction fiction=(Fiction) o;
-				return fiction.name;
-			}
-			else if(o instanceof TextBook){
-				TextBook text=(TextBook) o;
-				return text.subject;
-			}
-		}
-		return null;
-	}
 
-	public static void main(String[] args) {
-		
-		// TODO: Write a test code here and execute and text.
-		TextBook t = new TextBook();
-		System.out.println(getAgeOrTitle(t));
-		
-	}
+   
+    public static String getAgeOrTitle(Object o) {
+        if (!(o instanceof Book)) {
+            return "";
+        }
+        if (o instanceof Comic) {
+            return ((Comic) o).getTitle();
+        } else if (o instanceof Fiction) {
+            return ((Fiction) o).getName();
+        } else if (o instanceof TextBook) {
+            return ((TextBook) o).getSubject();
+        }
+        return "";
+    }
 
-	
+    public static void main(String[] args) {
+        TextBook t = new TextBook("Social Studies");
+        System.out.println("Subject of the TextBook: " + getAgeOrTitle(t)); 
+        
+        Comic c = new Comic("Dylan:The Great Adventures", 12);
+        System.out.println("Title of the Comic: " + getAgeOrTitle(c));
+        
+        Fiction f = new Fiction("Pulp Fiction", FictionType.Comedy);
+        System.out.println("Name of the Fiction: " + getAgeOrTitle(f));
+    }
 }
