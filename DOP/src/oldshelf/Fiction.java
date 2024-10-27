9d33c60676e94a551ef8616915a8850948c81659
PRATHYUSHCC <118652844+PRATHYUSHCC@users.noreply.github.com>
2024-10-27 13:02:38 +0530

    Update Fiction.java

--- DOP/src/oldshelf/Fiction.java
+++ DOP/src/oldshelf/Fiction.java
@@ -8,9 +8,11 @@ public class Fiction extends Book {
 	 * type is  a per instance object and it is initialized only once.
 	 * 
 	 */
+	private final String name;
 	private static FictionType type;
-	public Fiction(String title) {
+	public Fiction(String title,String name) {
 		super(title);
+		this.name=name;
 		// TODO  correct the above.
 	}
 
